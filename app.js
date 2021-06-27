4ea9438c11918d840d89568c706d63dbe189e7e1
EC2 Default User <ec2-user@ip-172-16-1-55.ec2.internal>
2021-06-27 23:20:31 +0000

    Cambia puerta

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello Youtube!"));
+app.get("/", (req, res) => res.send("Hello World 2!!"));
 
 app.get("/health", (req, res) => {
   res.status(200);
   res.send("healthy");
 });
 
-app.listen(3000, () => {
-  console.log("App listening on port 3000!");
+app.listen(8000, () => {
+  console.log("App listening on port 8000!");
 });
